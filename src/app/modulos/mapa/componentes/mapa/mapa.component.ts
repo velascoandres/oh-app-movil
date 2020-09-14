8d37fc80407cd8c68bb9f9bf40872af24dcc5cdc
andres velasco <velasco.andrs@gmail.com>
2020-09-14 18:33:29 -0500

    seleccionar icono

--- src/app/modulos/mapa/componentes/mapa/mapa.component.ts
+++ src/app/modulos/mapa/componentes/mapa/mapa.component.ts
@@ -1,16 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import Map from 'ol/Map';
 import TileLayer from 'ol/layer/Tile';
 import {OSM} from 'ol/source';
 import View from 'ol/View';
 import * as olProj from 'ol/proj';
 import {MapaService} from '../../servicios/mapa.service';
-import {Feature, Geolocation, Tile} from 'ol';
+import {Feature, Geolocation} from 'ol';
 import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import {Point} from 'ol/geom';
 import {Coordinate} from 'ol/coordinate';
 import XYZ from 'ol/source/XYZ';
+import {MapaAppState} from '../../store/mapa.store';
+import {Store} from '@ngrx/store';
+import {Draw, Interaction, Modify, Snap} from 'ol/interaction';
+import {Fill, Icon, Stroke, Style} from 'ol/style';
+import GeometryType from 'ol/geom/GeometryType';
+import BaseEvent from 'ol/events/Event';
+import IconAnchorUnits from 'ol/style/IconAnchorUnits';
+import CircleStyle from 'ol/style/Circle';
 
 
 @Component({
@@ -20,6 +28,9 @@ import XYZ from 'ol/source/XYZ';
 })
 export class MapaComponent implements OnInit {
 
+
+    @Input()
+    dibujarSoloUnaFigura = true;
     mapa: Map;
     geolocalizacion: Geolocation;
     caracteristicaPosicion: Feature;
@@ -27,10 +38,17 @@ export class MapaComponent implements OnInit {
     coordenadasActuales: Coordinate = [7.0785, 51.4614];
     source: XYZ;
     cargoMapa: boolean;
+    modificarInteraccion: any;
+    modificarVector: VectorLayer;
+    dibujarInteraccion: Interaction;
+    snapInteraccion: any;
+    lienzo: VectorSource;
 
     constructor(
         private readonly mapaService: MapaService,
+        private readonly mapaStore: Store<MapaAppState>,
     ) {
+        this.dibujarSoloUnaFigura = true;
     }
 
     ngOnInit() {
@@ -64,8 +82,11 @@ export class MapaComponent implements OnInit {
                 this.cargoMapa = true;
             }
         );
-        this.establecerGeolocalizacion();
         this.mapa.setSize([1000, 1000]);
+        this.establecerInteraccionModificar();
+        // Esta ultima funcion usarla en otra funcion para delegar que "forma" dibujar
+        this.agregarInteractionesParaDibujar();
+        this.establecerGeolocalizacion();
     }
 
     establecerGeolocalizacion() {
@@ -90,5 +111,99 @@ export class MapaComponent implements OnInit {
 
     centrarMapa() {
         this.vista.setCenter(this.coordenadasActuales);
+    }
+
+    escucharMapaStore() {
+        this.mapaStore
+            .select('mapa')
+            .subscribe(
+                ({poligonos, rutas, puntos}) => {
+                    if (puntos) {
+                        // dibujar puntos
+                    }
+                    if (poligonos) {
+                        // dibujar poligonos
+                    }
+                    if (rutas) {
+                        // dibujar rutas
+                    }
+                }
+            );
+    }
+
+    establecerInteraccionModificar() {
+        this.lienzo = new VectorSource();
+        this.modificarVector = new VectorLayer({
+            source: this.lienzo,
+            style: new Style({
+                fill: new Fill({
+                    color: 'rgba(255, 255, 255, 0.2)',
+                }),
+                stroke: new Stroke({
+                    color: '#ffcc33',
+                    width: 2,
+                }),
+                image: new Icon({
+                    anchor: [0.5, 200],
+                    src: 'assets/icon/casita.svg',
+                    anchorXUnits: IconAnchorUnits.FRACTION,
+                    anchorYUnits: IconAnchorUnits.PIXELS,
+                    scale: 0.09,
+                    opacity: 0.96,
+                }),
+            }),
+        });
+        this.modificarInteraccion = new Modify(
+            {
+                source: this.lienzo,
+                style: new Style(
+                    {
+                        image: new CircleStyle(
+                            {
+                                radius: 0,
+                                stroke: new Stroke(
+                                    {
+                                        color: 'rgba(255,255,255,0.9)'
+                                    }
+                                ),
+                            }
+                        )
+                    }
+                )
+            }
+        );
+        this.mapa.addInteraction(this.modificarInteraccion);
+        this.mapa.addLayer(this.modificarVector);
+    }
+
+    agregarInteractionesParaDibujar(tipo: GeometryType = GeometryType.POINT) {
+        this.dibujarInteraccion = new Draw({
+            source: this.lienzo,
+            type: tipo,
+        });
+        this.mapa.addInteraction(this.dibujarInteraccion);
+        this.snapInteraccion = new Snap({source: this.lienzo});
+        this.mapa.addInteraction(this.snapInteraccion);
+        this.dibujarInteraccion.on(
+            'drawend',
+            (evento: BaseEvent) => {
+                // Emitir coordenadas al store
+                if (this.dibujarSoloUnaFigura) {
+                    this.mapa.removeInteraction(this.dibujarInteraccion);
+                }
+            }
+        );
+    }
+
+    deshacerCambios() {
+        const caracteristicas = this.lienzo.getFeatures();
+        if (caracteristicas.length) {
+            const posicion = caracteristicas.length - 1;
+            const ultimaCaracteristica = caracteristicas[posicion];
+            if (posicion === 0) {
+                this.agregarInteractionesParaDibujar();
+            }
+            this.lienzo.removeFeature(ultimaCaracteristica);
+        }
     }
 }
